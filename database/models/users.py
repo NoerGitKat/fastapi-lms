c789ec318a558a363193d0e889de1713ff17b7e3
Noer Paanakker <noer@lightworksweb.nl>
2022-02-05 22:20:44 +0100

    added db model for users

---
+++ database/models/users.py
@@ -0,0 +1,35 @@
+import enum
+
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Enum, Text
+from sqlalchemy.orm import relationship
+
+from database.main import Base
+
+class Role(enum.Enum):
+    teacher = 1
+    student = 2
+
+# Models
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(100), unique=True, index=True, nullable=False)
+    role = Column(Enum(Role))
+
+    # Establish 1-1 relationship
+    profile = relationship("Profile", back_populates="owner", uselist=False)
+
+
+class Profile(Base):
+    __tablename__ ="profiles"
+
+     id = Column(Integer, primary_key=True, index=True)
+     first_name = Column(String(50), nullable=False)
+     last_name = Column(String(50), nullable=False)
+     bio  = Column(Text, nullable=True)
+     is_active = Column(Boolean, default=False)
+
+     # Establish 1-1 relationship
+     user_id = Column(Integer, ForeignKey("users.id"))
+     student = relationship("User", back_populates="profile")
